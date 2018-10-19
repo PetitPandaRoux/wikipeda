9f35c5750126e76cba78c50f693d4f16b2e39047
Sakada Ly <lysakada@gmail.com>
2018-10-19 17:18:31 +0200

    first public dir

---
+++ public/component---src-pages-contact-jsx-2594b836f9eb2edd7371.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{135:function(e,t,a){"use strict";a.r(t);var n=a(0),r=a.n(n),c=a(139);t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"Contact"}),r.a.createElement("p",null,"lysakada@gmail.com"))}},139:function(e,t,a){"use strict";var n=a(0),r=a.n(n);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-contact-jsx-2594b836f9eb2edd7371.js.map
