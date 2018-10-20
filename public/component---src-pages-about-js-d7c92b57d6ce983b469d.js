a1d5b4cc52cfc60ca55c387f332102e461dca0cd
Sakada Ly <lysakada@gmail.com>
2018-10-20 22:41:38 +0200

    suppression of public

--- public/component---src-pages-about-js-d7c92b57d6ce983b469d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{134:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(139);t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"A Propos"}),r.a.createElement("p",null,"Waouh"))}},139:function(e,t,n){"use strict";var a=n(0),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
-//# sourceMappingURL=component---src-pages-about-js-d7c92b57d6ce983b469d.js.map
